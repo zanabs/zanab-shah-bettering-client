bc8f0c93c99f6126a87a437ff34bfa1cd5368407
Zanab Shah <zanab@protonmail.com>
2024-09-14 15:51:28 -0700

    reworking header component

--- zanab-shah-bettering-client/src/components/Header.jsx
+++ zanab-shah-bettering-client/src/components/Header.jsx
@@ -1,29 +1,11 @@
 
 import'./Header.scss'
 
+
 const Header = () => {
-  return (
-    <header className=".header">
-      <div className="logo__container">
-        <div className="logo__wrapper">
-          <img 
-            src=""
-            alt="Company logo" 
-            className="logo" 
-            loading="lazy"
-          />
-        </div>
-      </div>
-      <button className="icon__button" label="Menu">
-        <img 
-          src="" 
-          alt="" 
-          className="icon__image"
-          loading="lazy"
-        />
-      </button>
-    </header>
-  );
-};
+  return 
+
+}
+    
 
 export default Header;
