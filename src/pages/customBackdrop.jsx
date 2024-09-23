23dfc39d99860eb72b4cdf8ab431287e3b94375a
Zanab Shah <zanab@protonmail.com>
2024-09-23 01:43:15 -0400

    setup home page to load categories

---
+++ src/pages/customBackdrop.jsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material/styles";
+import Backdrop from "@mui/material/Backdrop";
+
+
+const CustomBackdrop = styled(Backdrop)({
+//   gets rid of prebuilt backdrop
+});
+
+export default CustomBackdrop;
