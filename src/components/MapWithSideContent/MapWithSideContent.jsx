2f24f43da50f2d45a80f88b4bc1be876fc77829b
Zanab Shah <zanab@protonmail.com>
2024-09-23 21:22:50 -0400

    Create shared component for map + side content

---
+++ src/components/MapWithSideContent/MapWithSideContent.jsx
@@ -0,0 +1,35 @@
+import { Box } from "@mui/material"
+import { MapCard } from "../MapCard/MapCard"
+import './MapWithSideContent.scss';
+import FoodIcon from '@mui/icons-material/Restaurant';
+import MentalHealthIcon from '@mui/icons-material/Psychology';
+import GBVIcon from '@mui/icons-material/ReportProblem';
+import GardenIcon from '@mui/icons-material/Nature';
+import DentistryIcon from '@mui/icons-material/HealthAndSafety';
+import ShelterIcon from '@mui/icons-material/Home';
+import CulturalSupportIcon from '@mui/icons-material/Groups';
+
+const iconMapping = {
+    "food": <FoodIcon sx={{ color: 'brown' }} />,
+    "mental_health": <MentalHealthIcon sx={{ color: 'blue' }} />,
+    "gender_based_violence": <GBVIcon sx={{ color: 'pink' }} />,
+    "community_garden": <GardenIcon sx={{ color: 'green' }} />,
+    "dentistry": <DentistryIcon sx={{ color: 'purple' }} />,
+    "shelter": <ShelterIcon sx={{ color: 'orange' }} />,
+    "cultural_support": <CulturalSupportIcon sx={{ color: 'red' }} />
+  };
+
+export const MapWithSideContent = ({resources, children, showSideContent}) => {
+    return (
+        <section className="map-with-side-content">
+            <Box className="map-with-side-content__map">
+                <MapCard resources={resources} iconMapping={iconMapping} />
+            </Box>
+            {showSideContent && 
+                <Box className="map-with-side-content__content" sx={{p: 3, boxSizing: "border-box"}}>
+                    {children}
+                </Box>
+            }
+        </section>
+    )
+}
