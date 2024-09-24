7841c4ec00c13ed00fe7a7505b4a5750c7f204aa
Zanab Shah <zanab@protonmail.com>
2024-09-23 21:25:54 -0400

    Clean up home page component to only shpw landing page, remove extra pages

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import './HomePage.scss';
+import { useNavigate } from 'react-router-dom';
+
+export const HomePage = () => {
+  const navigate = useNavigate();
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    setKey(prevKey => prevKey + 1);
+  }, []);
+
+  return (
+    <section className="hero-section">
+      <video autoPlay loop muted playsInline className="hero-video">
+        <source src="../src/assets/HeroVideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="hero-content">
+
+        <h1 key={key} className="hero-heading">
+          Find access to social infrastructure, <span className="fast-typing">fast</span>
+        </h1>
+        <p className="hero-subtitle">Help is closer than you think</p>
+        <div className="hero-buttons">
+          <button className="hero-button" onClick={() => navigate('/city')}>Enter Site</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
--- src/pages/customBackdrop.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "@mui/material/styles";
-import Backdrop from "@mui/material/Backdrop";
-
-
-const CustomBackdrop = styled(Backdrop)({
-//   gets rid of prebuilt backdrop
-});
-
-export default CustomBackdrop;
