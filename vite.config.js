155efc8470419286bcb681efbae55b55d914ba2a
Zanab Shah <zanab@protonmail.com>
2024-09-25 01:37:50 -0700

    testing new vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCommonjs()],
-  optimizeDeps:{
-    include: ['axios']
-  },
-  build: {
-    commonjsOptions: {
-      include: ['/node_modules/axios']
-    }
-  }
+  // optimizeDeps:{
+  //   include: ['axios']
+  // },
+  // build: {
+  //   commonjsOptions: {
+  //     include: ['/node_modules/axios']
+  //   }
+  // }
 })
