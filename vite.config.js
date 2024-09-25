c9a427f54bd647fe080b74ca20ad58840907fe1c
Zanab Shah <zanab@protonmail.com>
2024-09-24 23:56:06 -0700

    use esbuildCommonJs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
+  plugins: [react()],
+  optimizeDeps:{
+    esbuildOptions:{
+      plugins:[
+        esbuildCommonjs()
+      ]
+    }
+  }
 })
