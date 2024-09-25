706c380876d54691a02245224d79eab0a3f9d37d
Zanab Shah <zanab@protonmail.com>
2024-09-24 23:09:28 -0700

    add vite common package to resolve rollup error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  rollupOptions: {
-    external: ['axios'],
-  }
+  plugins: [react(), viteCommonjs()],
 })
