710a42b4a45d7e33f900c2de64415ddb2bfa5cc1
Zanab Shah <zanab@protonmail.com>
2024-09-25 01:43:20 -0700

    testing new vite config with vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
-  // optimizeDeps:{
-  //   include: ['axios']
-  // },
-  // build: {
-  //   commonjsOptions: {
-  //     include: ['/node_modules/axios']
-  //   }
-  // }
+  plugins: [react(), viteCommonjs()]
 })
